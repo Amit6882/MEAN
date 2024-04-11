0cb1bdbc5041fbec8c5eb4bbbe59e24c17b4ff1b
Amit Dubey <akd6882@gmail.com>
2024-04-12 00:11:17 +0530

    Connected to DB

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
+const Post = require('./models/post')
 
 const app = express();
+
+mongoose.connect("mongodb+srv://akd6882:wVQiYI7rpCR0CLuv@akd6882.nhayi2j.mongodb.net/?retryWrites=true&w=majority&appName=akd6882")
+  .then(() => {
+    console.log('Connected to database!');
+  })
+  .catch(() => {
+    console.log('Connection failed!');
+  })
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,30 +31,40 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
+  // const post = req.body;
+  const post = new Post({
+    title: req.body.title,
+    content:req.body.content
+  });
   console.log(post);
+  post.save();
   res.status(201).json({
     message: 'Post added successfully'
   });
 });
 
 app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "fadf12421l",
-      title: "First server-side post",
-      content: "This is coming from the server"
-    },
-    {
-      id: "ksajflaj132",
-      title: "Second server-side post",
-      content: "This is coming from the server!"
-    }
-  ];
-  res.status(200).json({
-    message: "Posts fetched successfully!",
-    posts: posts
+  // const posts = [
+  //   {
+  //     id: "fadf12421l",
+  //     title: "First server-side post",
+  //     content: "This is coming from the server"
+  //   },
+  //   {
+  //     id: "ksajflaj132",
+  //     title: "Second server-side post",
+  //     content: "This is coming from the server!"
+  //   }
+  // ];
+
+  Post.find().then((documents) => {
+    console.log(documents);
+    res.status(200).json({
+      message: "Posts fetched successfully!",
+      posts: documents
+    });
   });
+
 });
 
 module.exports = app;
